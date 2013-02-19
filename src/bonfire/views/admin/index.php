dea09a8bae9ff46ccbbc02fbfd6d926bef082717
Lonnie Ezell <lonnieje@gmail.com>
2013-02-18 21:46:13 -0600

    Added placeholder admin home page.

---
+++ src/bonfire/views/admin/index.php
@@ -0,0 +1,9 @@
+<section class="intro">
+	<div class="container">
+
+		<h1>Welcome to Area 51</h1>
+
+		<p class="lead">This area would be used for displaying some useful info about<br/>this portion of the app.</p>
+
+	</div>
+</section>

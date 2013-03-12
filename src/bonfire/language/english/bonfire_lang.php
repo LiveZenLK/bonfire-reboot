fa787f1ab2a5bca375efcbb9c42586fe4353092b
Lonnie Ezell <lonnieje@gmail.com>
2013-03-12 00:17:25 -0500

    missed a couple of files

---
+++ src/bonfire/language/english/bonfire_lang.php
@@ -0,0 +1,6 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['bf_bonfire']		= 'Bonfire';
+$lang['bf_app']			= 'Application';
+$lang['bf_module']		= 'Module';
+$lang['bf_modules']		= 'Modules';
